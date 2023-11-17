5ff9ecf153db1275c46dadc5547f9657f2691205
Kade Robertson <kade@kaderobertson.dev>
2023-11-16 22:38:55 -0500

    chore: publish script

---
+++ scripts/publish-all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PKG_NAME=$(cat package.json | jq -r '.name')
+EXT_VERSION=$(cat package.json | jq -r '.version')
+
+pnpm submit \
+    --chrome-zip ".output/$PKG_NAME-$EXT_VERSION-chrome.zip" \
+    --firefox-zip ".output/$PKG_NAME-$EXT_VERSION-firefox.zip" \
+    --firefox-sources-zip ".output/$PKG_NAME-$EXT_VERSION-sources.zip"
